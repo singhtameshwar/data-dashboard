0cdbff032e5600ff7aba73ce8a6b4f497163167f
singhtameshwar <159401216+singhtameshwar@users.noreply.github.com>
2024-10-28 13:23:23 +0530

    making searchbar sticky

--- src/components/dashboard/heroheader.tsx
+++ src/components/dashboard/heroheader.tsx
@@ -2,7 +2,7 @@ import { FileText, ListFilter } from "lucide-react";
 
 export default function Header() {
   return (
-    <div className="flex flex-col lg:flex-row justify-between items-start">
+    <div className="flex flex-col lg:flex-row justify-between items-start z-10 p-4 shadow-md">
       <div className="ml-4 text-left">
         <h1 className="text-2xl lg:text-3xl font-bold">Your Heading</h1>
         <p className="text-sm lg:text-base text-gray-500">Your description goes here.</p>
